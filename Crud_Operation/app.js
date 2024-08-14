57613f15b1a0045224a1b7f8e981c300506a51ad
Waghre Abhishek <waghreabhishek@gmail.com>
2024-08-14 16:07:46 +0530

    Crud operation

---
+++ Crud_Operation/app.js
@@ -0,0 +1,72 @@
+
+
+const form = document.getElementById('crud-form')
+const itemInput = document.getElementById('item')
+const itemList = document.getElementById('item-list')
+
+let items = JSON.parse(localStorage.getItem('item')) || [];
+
+function saveItemToLocalStorage() {
+    localStorage.setItem('item', JSON.stringify(items))
+}
+
+// render items 
+function renderItems() {
+    itemList.innerHTML = '';
+    items.forEach((item, index) => {
+        const li = document.createElement('li')
+        li.textContent = item
+
+        const editButton = document.createElement('button')
+        editButton.textContent = 'Edit'
+        editButton.addEventListener('click', () => editItem(index))
+
+        const deleteButton = document.createElement('button')
+        deleteButton.textContent = 'Delete'
+        deleteButton.addEventListener('click', () => deleteItems(index))
+
+
+        li.appendChild(editButton)
+        li.appendChild(deleteButton)
+        itemList.appendChild(li)
+
+    });
+
+    saveItemToLocalStorage()
+}
+
+// create item list
+function createItem() {
+    const newItem = itemInput.value;
+    if (newItem) {
+        items.push(newItem)
+        itemInput.value = ''
+        saveItemToLocalStorage()
+        renderItems()
+
+    }
+}
+
+// editItem
+function editItem(index) {
+    const updateItem = prompt('edit item:', items[index])
+    if (updateItem !== null) {
+        items[index] = updateItem
+        renderItems()
+    }
+}
+
+// delete item
+function deleteItems(index) {
+    items.splice(index, 1)
+    renderItems()
+}
+
+// on submit item 
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createItem()
+})
+
+
+renderItems()
